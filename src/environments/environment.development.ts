feaad2f436909dcd1a16d5142233ea5b8864d4bf
Sun9990 <sunetradutta93@gmail.com>
2023-12-20 15:08:43 +0530

    report category added

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -8,6 +8,7 @@ export const environment = {
     hostName:'http://127.0.0.1:5000',
     socketName:'http://127.0.0.1:5000',
     
+    
     // production: true,
     // hostName: '/api',
     // socketName : '/',
