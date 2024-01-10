a33b72ddce897b7db7168218eb728140acb1da59
Priyadarshi Roy <proy9714@gmail.com>
2024-01-10 11:53:53 +0530

    Updated angular.json for production

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,119 @@
+export const environment = {
+  hostName: 'http://127.0.0.1:5000',
+  socketName: 'http://127.0.0.1:5000',
+  razorpay_key: 'rzp_test_6nso0FMZS1eI1K',
+
+  seoData: {
+    home: {
+      title: 'Texplici2 - Home',
+      metaTags: [
+        { name: 'description', content: "Build your Knowledge Management System" },
+        { property: 'og:title', content: 'Texplici2 - Home' },
+        { proprety: 'og:description', content: "Build your Knowledge Management System" },
+        { property: 'og:image', content: 'http://localhost:4200/assets/images/seo/home.png' },
+        { property: 'og:url', content: 'http://localhost:4200/home' }
+      ]
+    },
+    mydocuments: {
+      title: 'Texplici2 - My Documents',
+      metaTags: [
+        { name: 'description', content: 'About Texplicit 02, a Latin scholar from Hampden-Sydney College, is credited with discovering.' },
+        { property: 'og:title', content: 'Texplici2 - My Netrok' },
+        { property: 'og:description', content: 'My Netrok Texplicit 02' },
+        { property: 'og:image', content: '*' },
+        { property: 'og:url', content: '*' }
+      ]
+    },
+    documentItemizedSummary: {
+      title: 'Texplici2 - My Documents Itemized Summary',
+      metaTags: [
+        { name: 'description', content: "Build your Knowledge Management System" },
+        { property: 'og:title', content: 'Texplici2 - Home' },
+        { proprety: 'og:description', content: "Build your Knowledge Management System" },
+        { property: 'og:image', content: 'http://localhost:4200/assets/images/seo/home.png' },
+        { property: 'og:url', content: 'http://localhost:4200/home' }
+      ]
+    },
+    documentHighlight: {
+      title: 'Texplici2 - My Documents Highlights',
+      metaTags: [
+        { name: 'description', content: "Build your Knowledge Management System" },
+        { property: 'og:title', content: 'Texplici2 - Home' },
+        { proprety: 'og:description', content: "Build your Knowledge Management System" },
+        { property: 'og:image', content: 'http://localhost:4200/assets/images/seo/home.png' },
+        { property: 'og:url', content: 'http://localhost:4200/home' }
+      ]
+    },
+    registration: {
+      title: 'Texplici2 - User Registration',
+      metaTags: [
+        { name: 'description', content: 'About Texplicit 02, a Latin scholar from Hampden-Sydney College, is credited with discovering.' },
+        { property: 'og:title', content: 'Texplici2 - My Netrok' },
+        { property: 'og:description', content: 'My Netrok Texplicit 02' },
+        { property: 'og:image', content: '*' },
+        { property: 'og:url', content: '*' }
+      ]
+    },
+    resetpassword: {
+      title: 'Texplici2 - Reset Password',
+      metaTags: [
+        { name: 'description', content: 'About Texplicit 02, a Latin scholar from Hampden-Sydney College, is credited with discovering.' },
+        { property: 'og:title', content: 'Texplici2 - My Netrok' },
+        { property: 'og:description', content: 'My Netrok Texplicit 02' },
+        { property: 'og:image', content: '*' },
+        { property: 'og:url', content: '*' }
+      ]
+    },
+    reports: {
+      title: 'Texplici2 - Reports',
+      metaTags: [
+        { name: 'description', content: 'About Texplicit 02, a Latin scholar from Hampden-Sydney College, is credited with discovering.' },
+        { property: 'og:title', content: 'Texplici2 - My Netrok' },
+        { property: 'og:description', content: 'My Netrok Texplicit 02' },
+        { property: 'og:image', content: '*' },
+        { property: 'og:url', content: '*' }
+      ]
+    },
+    manageuser: {
+      title: 'Texplici2 - Manage User',
+      metaTags: [
+        { name: 'description', content: 'About Texplicit 02, a Latin scholar from Hampden-Sydney College, is credited with discovering.' },
+        { property: 'og:title', content: 'Texplici2 - My Netrok' },
+        { property: 'og:description', content: 'My Netrok Texplicit 02' },
+        { property: 'og:image', content: '*' },
+        { property: 'og:url', content: '*' }
+      ]
+    },
+    aboutus: {
+      title: 'Texplici2 - About',
+      metaTags: [
+        { name: 'description', content: 'About Texplicit 02, a Latin scholar from Hampden-Sydney College, is credited with discovering.' },
+        { property: 'og:title', content: 'Texplici2 - About' },
+        { property: 'og:description', content: 'About Texplicit 02' },
+        { property: 'og:image', content: 'http://localhost:4200/assets/images/seo/about-us.png' },
+        { property: 'og:url', content: 'http://localhost:4200/about-us' }
+      ]
+    },
+    contactus: {
+      title: 'Texplici2 - Contact',
+      metaTags: [
+        { name: 'description', content: 'Get in touch with us for more Information' },
+        { property: 'og:title', content: 'Texplici2 - Contact' },
+        { proprety: 'og:description', content: 'Get in touch with us for more Information' },
+        { property: 'og:image', content: 'http://localhost:4200/assets/images/seo/contact-us.png' },
+        { property: 'og:url', content: 'http://localhost:4200/contact-us' }
+      ]
+    },
+    admin: {
+      title: 'Texplici2 - Manage User',
+      metaTags: [
+        { name: 'description', content: 'About Texplicit 02, a Latin scholar from Hampden-Sydney College, is credited with discovering.' },
+        { property: 'og:title', content: 'Texplici2 - My Netrok' },
+        { property: 'og:description', content: 'My Netrok Texplicit 02' },
+        { property: 'og:image', content: '*' },
+        { property: 'og:url', content: '*' }
+      ]
+    },
+  }
+};
+
