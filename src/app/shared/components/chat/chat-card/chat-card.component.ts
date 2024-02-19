70287deca70fe7f2e353856d5494db2c6f7dd0d4
Priyadarshi Roy <proy9714@gmail.com>
2024-02-19 18:04:26 +0530

    Added indicator for type of chat

--- src/app/shared/components/chat/chat-card/chat-card.component.ts
+++ src/app/shared/components/chat/chat-card/chat-card.component.ts
@@ -8,4 +8,8 @@ import { Component, Input } from '@angular/core';
 export class ChatCardComponent {
   @Input() chats: any = [];
   @Input() userImage: string = "";
+
+  getChatTypeString(chatType: number): string {
+    return chatType === 0 ? 'External' : chatType === 1 ? 'My Documents' : '';
+  }
 }
