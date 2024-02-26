d88b11570c75310a143221c8fc66a47882063887
Priyadarshi Roy <proy9714@gmail.com>
2024-02-26 15:44:34 +0530

    Setting up form structure and validations for email sharing

--- src/app/my-documents/modal-dialog/file-folder-share-dialog/file-folder-share-dialog.component.ts
+++ src/app/my-documents/modal-dialog/file-folder-share-dialog/file-folder-share-dialog.component.ts
@@ -1,15 +1,13 @@
-import { Component, Inject, inject } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { CommonService } from 'src/app/shared/services/common.service';
-import { MydocumentsService } from 'src/app/services/mydocuments.service';
-import { LocalStorageService } from 'src/app/core/local-storage.service';
-import { Observable } from 'rxjs';
-import { COMMA, ENTER } from '@angular/cdk/keycodes';
-import { MatChipEditedEvent, MatChipInputEvent } from '@angular/material/chips';
-import { MatIconModule } from '@angular/material/icon';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { Component, Inject, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatChipEditedEvent, MatChipInputEvent } from '@angular/material/chips';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { LocalStorageService } from 'src/app/core/local-storage.service';
+import { MydocumentsService } from 'src/app/services/mydocuments.service';
+import { CommonService } from 'src/app/shared/services/common.service';
 
 export interface Email {
   address: string;
@@ -19,10 +17,9 @@ export interface Email {
 @Component({
   selector: 'app-file-folder-share-dialog',
   templateUrl: './file-folder-share-dialog.component.html',
-  styleUrls: ['./file-folder-share-dialog.component.scss']
+  styleUrls: ['./file-folder-share-dialog.component.scss'],
 })
 export class FileFolderShareDialogComponent {
-  // items:any=Array(10);
   currId: any;
   docId: string = "";
   document: any;
@@ -31,14 +28,13 @@ export class FileFolderShareDialogComponent {
   filteredUsers: any;
   userInfo$: Observable<any> = this.localStorageService.userInfo$;
 
-
-
   addOnBlur = true;
   readonly separatorKeysCodes = [ENTER, COMMA] as const;
-  emails: Email[] = [{ address: 'email1@example.com' }, { address: 'email2@example.com' }];
+  emails: Email[] = [];
 
   announcer = inject(LiveAnnouncer);
 
+  emailShareForm: FormGroup;
 
   constructor(
     public dialogRef: MatDialogRef<FileFolderShareDialogComponent>,
@@ -47,7 +43,16 @@ export class FileFolderShareDialogComponent {
     private mydocs: MydocumentsService,
     private localStorageService: LocalStorageService,
     private commonservice: CommonService
-  ) { }
+  ) {
+    this.emailShareForm = this.formBuilder.group({
+      emailIds: new FormControl([""], [Validators.required, Validators.email]),
+      subject: new FormControl(""),
+      message: new FormControl(""),
+      shareType: new FormControl("")
+    });
+  }
+
+
   ngOnInit() {
     this.localStorageService.observeUserInfo();
     this.userInfo$.subscribe((data) => {
@@ -60,13 +65,13 @@ export class FileFolderShareDialogComponent {
   }
 
 
-
   add(event: MatChipInputEvent): void {
     const value = (event.value || '').trim();
 
     // Add our email
     if (value) {
       this.emails.push({ address: value });
+      this.updateEmailsFormControl();
     }
 
     // Clear the input value
@@ -78,12 +83,13 @@ export class FileFolderShareDialogComponent {
 
     if (index >= 0) {
       this.emails.splice(index, 1);
+      this.updateEmailsFormControl();
 
       this.announcer.announce(`Removed ${email.address}`);
     }
   }
 
-  edit(email: Email, event: MatChipEditedEvent) {
+  edit(email: Email, event: MatChipEditedEvent): void {
     const value = event.value.trim();
 
     // Remove email if it no longer has an address
@@ -96,9 +102,14 @@ export class FileFolderShareDialogComponent {
     const index = this.emails.indexOf(email);
     if (index >= 0) {
       this.emails[index].address = value;
+      this.updateEmailsFormControl();
     }
   }
 
+  private updateEmailsFormControl(): void {
+    this.emailShareForm.get('emailIds')?.patchValue(this.emails.map(email => email.address));
+    this.emailShareForm.get('emailIds')?.updateValueAndValidity();
+  }
 
   getDocument() {
     console.log("DocId:", this.docId);
@@ -116,6 +127,7 @@ export class FileFolderShareDialogComponent {
       }
     });
   }
+
   getAllUsers() {
     this.commonservice.getAllUsers().subscribe({
       next: (response: any) => {
@@ -132,6 +144,7 @@ export class FileFolderShareDialogComponent {
       }
     });
   }
+
   searchUsers(value: string): void {
     this.userSearchQuery = value;
     if (this.userSearchQuery) {
@@ -141,22 +154,28 @@ export class FileFolderShareDialogComponent {
       this.filteredUsers = this.users;
     }
   }
+
   clearUserSearch() {
     this.userSearchQuery = '';
     this.filteredUsers = this.users;
   }
+
   hasAccess(userId: string) {
-    console.log("Document : ", this.document);
     let accessList = "usersWithAccess" in this.document ? this.document.usersWithAccess : [];
-    console.log("AccessList : ", accessList);
     if (accessList == null) {
       accessList = [];
     }
 
     return accessList.includes(userId);
   }
-  shareDocument(userId: string): void {
-    this.mydocs.shareDocument(this.docId, [userId]).subscribe({
+
+  shareToInternal(userId: string) {
+    this.emailShareForm.get('shareType')?.patchValue("internal");
+    this.shareDocument(userId);
+  }
+
+  shareDocument(userId: string = ""): void {
+    this.mydocs.shareDocument(this.docId, [userId], this.emailShareForm.value).subscribe({
       next: (response: any) => {
         console.log("Response:", response);
         this.commonservice.showSnackbar("snackbar-success", response.message, "0");
@@ -171,7 +190,28 @@ export class FileFolderShareDialogComponent {
     });
     console.log(userId);
   }
+
   onCloseClick() {
     this.dialogRef.close(false);
+  }
+
+  getEmailsErrorMessage() {
+    const emailFormControl = this.emailShareForm.get('emails');
+
+    if (emailFormControl?.hasError('required')) {
+      return 'At least one email id is required!';
+    }
+
+    return emailFormControl?.hasError('email') ? 'An invalid email address was entered!' : '';
+  }
+
+
+  onSubmit() {
+    if (this.emailShareForm.valid) {
+      console.log("form : ", this.emailShareForm.value);
+      this.emailShareForm.get('shareType')?.patchValue("email");
+      // this.dialogRef.close(this.emailShareForm.value);
+      this.shareDocument();
+    }
   }
 }
--- src/app/services/mydocuments.service.ts
+++ src/app/services/mydocuments.service.ts
@@ -2,72 +2,84 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import {saveAs} from 'file-saver';
+import { saveAs } from 'file-saver';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MydocumentsService {
-  // private api = "http://115.187.43.48:6544"
+  constructor(private http: HttpClient) { }
 
-  constructor(private http:HttpClient) {}
-  createFolder(input:any) {
-    console.log("Input:",input);
+  createFolder(input: any) {
+    console.log("Input:", input);
     const url = `${environment.hostName}/my-documents/create-folder`;
-    return this.http.post<any>(url,input);
+    return this.http.post<any>(url, input);
   }
-  getAllFiles(root : string, limit: number = 20, offset: number = 0): Observable<any> {
+
+  getAllFiles(root: string, limit: number = 20, offset: number = 0): Observable<any> {
     const url = `${environment.hostName}/my-documents/display-documents`;
-    const params = {"root" : root, "limit": limit, "offset": offset};
+    const params = { "root": root, "limit": limit, "offset": offset };
     console.log("params : ", params);
-    return this.http.get<any>(url, {params});
+    return this.http.get<any>(url, { params });
   }
-  uploadFiles(formData : FormData): Observable<any> {
+
+  uploadFiles(formData: FormData): Observable<any> {
     const url = `${environment.hostName}/my-documents/upload-documents`;
-    return this.http.post<any>(url,formData,{reportProgress:true,observe:'events'});
+    return this.http.post<any>(url, formData, { reportProgress: true, observe: 'events' });
   }
-  deleteFolder(folderId : string , path:string): Observable<any>{
+
+  deleteFolder(folderId: string, path: string): Observable<any> {
     const url = `${environment.hostName}/my-documents/delete-folder/${folderId}`
     return this.http.delete<any>(url);
   }
-  deleteFile(fileId: string , path:string): Observable<any>{
+
+  deleteFile(fileId: string, path: string): Observable<any> {
     const url = `${environment.hostName}/my-documents/${fileId}`
     return this.http.delete<any>(url);
   }
-  movefile(file: any , folder: any){
+
+  movefile(file: any, folder: any) {
     const url = `${environment.hostName}/my-documents/move-files`
-    const params = {"file":file,"folder":folder};
-    console.log("params:",params);
-    return this.http.put<any>(url,params);
+    const params = { "file": file, "folder": folder };
+    console.log("params:", params);
+    return this.http.put<any>(url, params);
   }
-  getDocument(documentId: string): Observable<any>{
+
+  getDocument(documentId: string): Observable<any> {
     const url = `${environment.hostName}/my-documents/${documentId}`;
     return this.http.get<any>(url);
   }
-  shareDocument(documentId: string, usersWithAccess: any): Observable<any> {
+
+  shareDocument(documentId: string, usersWithAccess: any, emailShareForm: any): Observable<any> {
     const url = `${environment.hostName}/my-documents/share`;
     const params = {
       "documentId": documentId,
-      "usersWithAccess": usersWithAccess
+      "usersWithAccess": usersWithAccess,
+      "emailIds": emailShareForm.emailIds,
+      "subject": emailShareForm.subject,
+      "message": emailShareForm.message
     };
     return this.http.post<any>(url, params);
   }
-  getAllFolders():Observable<any>{
+
+  getAllFolders(): Observable<any> {
     const url = `${environment.hostName}/my-documents/display-folders`;
     console.log("Url : ", url);
     return this.http.get<any>(url);
   }
-  getFolderContents(_id : string) : Observable<any> {
+
+  getFolderContents(_id: string): Observable<any> {
     const url = `${environment.hostName}/my-documents/folder-content/${_id}`;
     return this.http.get<any>(url);
   }
-  downloadFile(virtualFileName:string,originalFileName:string){
+
+  downloadFile(virtualFileName: string, originalFileName: string) {
     const url = `${environment.hostName}/my-documents/download/${virtualFileName}`;
     // const params = {"root":strpath}
-    this.http.get(url, {responseType: 'blob'}).subscribe({
+    this.http.get(url, { responseType: 'blob' }).subscribe({
       next: (res: any) => {
         console.log(res);
-        let blob: any = new Blob([res], {type: 'text/json; charset=utf-8'});
+        let blob: any = new Blob([res], { type: 'text/json; charset=utf-8' });
         const url = window.URL.createObjectURL(blob);
         saveAs(blob, originalFileName);
         console.log("File download complete");
@@ -78,14 +90,15 @@ export class MydocumentsService {
       }
     });
   }
-  rename(id:string,newname:string){
-    console.log("Id",id);
-    console.log("Rename_value:",newname);
+
+  rename(id: string, newname: string) {
+    console.log("Id", id);
+    console.log("Rename_value:", newname);
     const url = `${environment.hostName}/my-documents/rename/${id}`;
     const params = {
-      "renameValue":newname
+      "renameValue": newname
     };
-    return this.http.put<any>(url,params);
+    return this.http.put<any>(url, params);
   }
   // shareFolder(documentId: string, usersWithAccess: any): Observable<any> {
   //   const url = `${environment.hostName}/my-documents/share`;
