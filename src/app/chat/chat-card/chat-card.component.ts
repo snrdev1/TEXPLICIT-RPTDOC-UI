903acc7ae4dc680004caab41f6290fa2436166cc
Priyadarshi Roy <proy9714@gmail.com>
2024-03-06 15:07:43 +0530

    Initial design changes completed for chat page

--- src/app/chat/chat-card/chat-card.component.ts
+++ src/app/chat/chat-card/chat-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Clipboard } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-chat-card',
@@ -9,7 +10,15 @@ export class ChatCardComponent {
   @Input() chats: any = [];
   @Input() userImage: string = "";
 
+  constructor(
+    private clipboard: Clipboard
+  ) { }
+
   getChatTypeString(chatType: number): string {
     return chatType === 0 ? 'External' : chatType === 1 ? 'My Documents' : '';
+  }
+
+  copyChat(text: string) {
+    this.clipboard.copy(text);
   }
 }
