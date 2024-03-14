5b19857cef49db18bd6edc5463292b24b635d7be
Sun9990 <sunetradutta93@gmail.com>
2024-03-14 18:23:17 +0530

    addUser added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   // hostName: '/api',
   // socketName : '/',
-  hostName: 'http://127.0.0.1:5000',
-  socketName: 'http://127.0.0.1:5000',
+  hostName: 'http://127.0.0.1:8080',
+  socketName: 'http://127.0.0.1:8080',
   razorpay_key: 'rzp_test_6nso0FMZS1eI1K',
 
   seoData: {
